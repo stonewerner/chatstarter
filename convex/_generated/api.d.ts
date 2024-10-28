b07e10001b8a318dcb4b5ee03b157a5ff35a0fe6
Stone Werner <stonewerner@berkeley.edu>
2024-10-28 17:01:10 -0500

    33:24 in video

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as functions_message from "../functions/message.js";
+import type * as http from "../http.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -25,6 +26,7 @@ import type * as functions_message from "../functions/message.js";
  */
 declare const fullApi: ApiFromModules<{
   "functions/message": typeof functions_message;
+  http: typeof http;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
