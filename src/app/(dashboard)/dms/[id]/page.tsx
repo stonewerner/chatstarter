a1d8edda89145a7eb7b2a7ada7abae31b0cbb2e3
Stone Werner <stonewerner@berkeley.edu>
2024-11-02 10:50:46 -0500

    typing indicators done 1:27:16

--- src/app/(dashboard)/dms/[id]/page.tsx
+++ src/app/(dashboard)/dms/[id]/page.tsx
@@ -47,7 +47,25 @@ export default function MessagePage({
           <MessageItem key={message._id} message={message} />
         ))}
       </ScrollArea>
+      <TypingIndicator directMessage={id} />
       <MessageInput directMessage={id} />
+    </div>
+  );
+}
+
+function TypingIndicator({
+  directMessage,
+}: {
+  directMessage: Id<"directMessages">;
+}) {
+  const usernames = useQuery(api.functions.typing.list, { directMessage });
+
+  if (!usernames || usernames.length === 0) {
+    return null;
+  }
+  return (
+    <div className="text-sm text-muted-foreground px-4 py-2">
+      {usernames.join(", ")} is typing...
     </div>
   );
 }
@@ -104,6 +122,7 @@ function MessageInput({
 }) {
   const [content, setContent] = useState("");
   const sendMessage = useMutation(api.functions.message.create);
+  const sendTypingIndicator = useMutation(api.functions.typing.upsert);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -124,6 +143,11 @@ function MessageInput({
         placeholder="Message"
         value={content}
         onChange={(e) => setContent(e.target.value)}
+        onKeyDown={(e) => {
+          if (content.length > 0) {
+            sendTypingIndicator({ directMessage });
+          }
+        }}
       />
       <Button size="icon">
         <SendIcon />
