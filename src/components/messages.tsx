14a119796eab549947ee64cb0f11f23f5716480b
Stone Werner <stonewerner@berkeley.edu>
2024-11-08 16:40:21 -0600

    43 min into part3

---
+++ src/components/messages.tsx
@@ -0,0 +1,216 @@
+import { Id } from "../../convex/_generated/dataModel";
+import { FunctionReturnType } from "convex/server";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import Image from "next/image";
+import { api } from "../../convex/_generated/api";
+import { useMutation, useQuery } from "convex/react";
+import { ScrollArea } from "./ui/scroll-area";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  LoaderIcon,
+  MoreVerticalIcon,
+  PlusIcon,
+  SendIcon,
+  TrashIcon,
+} from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useRef, useState } from "react";
+
+export function Messages({ id }: { id: Id<"directMessages" | "channels"> }) {
+  const messages = useQuery(api.functions.message.list, {
+    dmOrChannelId: id,
+  });
+  return (
+    <>
+      <ScrollArea className="h-full py-4">
+        {messages?.map((message) => (
+          <MessageItem key={message._id} message={message} />
+        ))}
+      </ScrollArea>
+      <TypingIndicator id={id} />
+      <MessageInput id={id} />
+    </>
+  );
+}
+
+function TypingIndicator({ id }: { id: Id<"directMessages" | "channels"> }) {
+  const usernames = useQuery(api.functions.typing.list, { dmOrChannelId: id });
+
+  if (!usernames || usernames.length === 0) {
+    return null;
+  }
+  return (
+    <div className="text-sm text-muted-foreground px-4 py-2">
+      {usernames.join(", ")} is typing...
+    </div>
+  );
+}
+
+type Message = FunctionReturnType<typeof api.functions.message.list>[number];
+
+function MessageItem({ message }: { message: Message }) {
+  return (
+    <div className="flex items-center px-4 gap-2 py-2">
+      <Avatar className="size-8 border">
+        {message.sender && <AvatarImage src={message.sender.image} />}
+        <AvatarFallback />
+      </Avatar>
+      <div className="flex flex-col mr-auto">
+        <p className="text-xs text-muted-foreground">
+          {message.sender?.username ?? "Deleted User"}
+        </p>
+        <p className="text-sm">{message.content}</p>
+        {message.attachment && (
+          <Image
+            src={message.attachment}
+            alt="Attachment"
+            width={300}
+            height={300}
+            className="rounded border overflow-hidden"
+          />
+        )}
+      </div>
+      <MessageActions message={message} />
+    </div>
+  );
+}
+
+function MessageActions({ message }: { message: Message }) {
+  const user = useQuery(api.functions.user.get);
+  const removeMutation = useMutation(api.functions.message.remove);
+  if (!user || message.sender?._id !== user._id) {
+    return null;
+  }
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <MoreVerticalIcon className="size-4 text-muted-foreground" />
+        <span className="sr-only">Message Actions</span>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem
+          className="text-destructive"
+          onClick={() => removeMutation({ id: message._id })}
+        >
+          <TrashIcon />
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+function MessageInput({ id }: { id: Id<"directMessages" | "channels"> }) {
+  const [content, setContent] = useState("");
+  const sendMessage = useMutation(api.functions.message.create);
+  const sendTypingIndicator = useMutation(api.functions.typing.upsert);
+  const generateUploadUrl = useMutation(
+    api.functions.storage.generateUploadUrl
+  );
+  const [attachment, setAttachment] = useState<Id<"_storage">>();
+  const [file, setFile] = useState<File>();
+  const [isUploading, setIsUploading] = useState(false);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleImageUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+    setFile(file);
+    setIsUploading(true);
+    const url = await generateUploadUrl();
+    const res = await fetch(url, {
+      method: "POST",
+      body: file,
+    });
+    const { storageId } = (await res.json()) as { storageId: Id<"_storage"> };
+    setAttachment(storageId);
+    setIsUploading(false);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await sendMessage({ dmOrChannelId: id, attachment, content });
+      setContent("");
+      setAttachment(undefined);
+      setFile(undefined);
+    } catch (error) {
+      toast.error("Failed to send message", {
+        description:
+          error instanceof Error ? error.message : "An unknown error occurred",
+      });
+    }
+  };
+
+  return (
+    <>
+      <form className="flex items-end p-4 gap-2" onSubmit={handleSubmit}>
+        <Button
+          type="button"
+          size="icon"
+          onClick={() => {
+            fileInputRef.current?.click();
+          }}
+        >
+          <PlusIcon />
+          <span className="sr-only">Attach</span>
+        </Button>
+        <div className="flex flex-col flex-1 gap-2">
+          {file && <ImagePreview file={file} isUploading={isUploading} />}
+          <Input
+            placeholder="Message"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            onKeyDown={() => {
+              if (content.length > 0) {
+                sendTypingIndicator({ dmOrChannelId: id });
+              }
+            }}
+          />
+        </div>
+
+        <Button size="icon">
+          <SendIcon />
+          <span className="sr-only">Send</span>
+        </Button>
+      </form>
+      <input
+        type="file"
+        className="hidden"
+        ref={fileInputRef}
+        onChange={handleImageUpload}
+      />
+    </>
+  );
+}
+
+function ImagePreview({
+  file,
+  isUploading,
+}: {
+  file: File;
+  isUploading: boolean;
+}) {
+  return (
+    <div className="relative size-40 overflow-hidden rounded border">
+      <Image
+        src={URL.createObjectURL(file)}
+        alt="Attachment"
+        width={300}
+        height={300}
+      />
+      {isUploading && (
+        <div className="absolute inset-0 flex items-center justify-center bg-background/50">
+          <LoaderIcon className="animate-spin size-8" />
+        </div>
+      )}
+    </div>
+  );
+}
